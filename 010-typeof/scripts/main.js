c872d913fffba17ace965519b8598692a6244a70
Rio Waller <riowaller@gmail.com>
2023-02-07 09:51:01 -0800

    more on variables

--- 010-typeof/scripts/main.js
+++ 010-typeof/scripts/main.js
@@ -1,8 +1,24 @@
-let myName = "Rio"
+let myName = "59"
 let myAge = 59
 let myHappiness = true
+let newVar = 'SOMETHING'
+const myConst = 42
+console.log(myAge == myName)
+console.log(myAge === myName)
+
+let myVar = null
+console.log(`myVar is typeof ${typeof myVar}`)
 
 console.log(`myName is typeof ${typeof myName}`)
+console.log(`myName is value ${myName}`)
 console.log(`myAge is typeof ${typeof myAge}`)
 console.log(`myHappiness is typeof ${typeof myHappiness}`)
+
+// don't use var 
+// if (true) {
+//     const myConst = 43
+//     console.log(myConst)
+// } else {
+
+// }
 
