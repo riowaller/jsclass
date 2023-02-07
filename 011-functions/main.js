d77458232c624c0847ca8e841901d765b8cb09a1
Rio Waller <riowaller@gmail.com>
2023-02-07 10:51:41 -0800

    fun with functions

---
+++ 011-functions/main.js
@@ -0,0 +1,10 @@
+//Functions are fun!
+
+function doMath (a, b) {
+    console.log(`this is the result ${a - b}`)
+    console.log(`this is the result ${a + b}`)
+    console.log(`this is the result ${a / b}`)
+    console.log(`this is the result ${a * b}`)
+}
+
+doMath(10, 4)
