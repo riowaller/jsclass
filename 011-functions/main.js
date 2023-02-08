53ea6ccd5d531bdeabed44278f1d44b5c35f98e0
Rio Waller <riowaller@gmail.com>
2023-02-08 09:24:36 -0800

    adding changes

--- 011-functions/main.js
+++ 011-functions/main.js
@@ -9,5 +9,14 @@ function doMath (a, b) {
     // console.log(`this is the result ${a * b}`)
 }
 
+nowdoMoreMath = (a, b) => a * b
+ 
+
+
+console.log(doMoreMath(20, 30))
+
+
 const result = doMath(10, 4)
 console.log(`this result was returned from the function  ${result}`)
+
+ 
