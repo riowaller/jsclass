d592806a673a80471342c936463766ff591bff66
Rio Waller <riowaller@gmail.com>
2023-02-13 14:21:46 -0800

    Week 7 Part 1 files EOA

---
+++ 012-more-functions/README.md
@@ -0,0 +1,4 @@
+# Week 7
+
+# Are you using devTools?
+
---
+++ 012-more-functions/main.js
@@ -0,0 +1,21 @@
+//Update the DOM
+
+const myArr = []
+
+const updateDOM = (input) => {
+    const divEl = document.querySelector('#output')
+    const p = document.createElement('p')
+    p.textContent = input
+    divEl.appendChild(p)
+}
+
+const trackMPGCost = (miles, gallons, price = 3.79) => {
+    const MPG  = Math.round(miles/gallons)
+    const tripCost = gallons * price
+    updateDOM(`Miles per gallon  is ${MPG} and trip cost is ${tripCost}`)
+    myArr.push(MPG, tripCost)
+}
+
+trackMPGCost(360, 15, 5.40)
+trackMPGCost(320, 12, 5)
+updateDOM(myArr)
